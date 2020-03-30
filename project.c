92f1bc635295d598fcb1bf059c92c95899b84dd8
shagunagra <59168880+shagunagra@users.noreply.github.com>
2020-03-30 06:39:41 -0700

    Update project.c
    Added additional things

--- project.c
+++ project.c
@@ -1,41 +1,41 @@
 #include<unistd.h>
 #include<pthread.h>
 #include<stdio.h>
-int goal=0;
-long fib[500];
+int a=0;
+long f[500];
 int i;
-void *runner(void *param)
+void *fun(void *b)
 {
-if(goal<=0)
+if(a<=0)
 pthread_exit(0);
-fib[0]=0;
-if(goal>1)
+f[0]=0;
+if(a>1)
 {
-fib[1]=1;
-for(i=2;i<goal;i++)
-fib[i]=fib[i-1]+fib[i-2];
+f[1]=1;
+for(i=2;i<a;i++)
+f[i]=f[i-1]+f[i-2];
 }
 pthread_exit(0);
 }
 
-int main(int argc, char *arvg[])
+int main(int c, char *cf[])
 {
-pthread_t tid;
+pthread_t id;
 pthread_attr_t attr;
 pthread_attr_init(&attr);
-printf("Print this many Fibonacci numbers: ");
-scanf("%d",&goal);
-if(goal>500)
+printf("Enter the number of Fibonacci series: ");
+scanf("%d",&a);
+if(a>500)
 {
-printf("Printing as many as possible: 500\n");
-goal=500;
+printf("Printing upto 500 only");
+a=500;
 }
-pthread_create(&tid, &attr, runner, arvg[1]);
-pthread_join(tid,NULL);
-if(goal>0)
-printf("%ld",fib[0]);
-for(i=1;i<goal;i++)
-printf(",%ld",fib[i]);
+pthread_create(&id, &attr, fun, cf[1]);
+pthread_join(id,NULL);
+if(a>0)
+printf("%ld",f[0]);
+for(i=1;i<a;i++)
+printf(",%ld",f[i]);
 printf("\n");
 return 0;
 }
