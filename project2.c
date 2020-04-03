58b634083ca0a0c1523d62235d8500a2de4e8ce3
shagunagra <59168880+shagunagra@users.noreply.github.com>
2020-04-02 23:25:21 -0700

    changes in code

--- project2.c
+++ project2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-double page_fault_rate();
-void userInput(void);
+double pfr();
+void ui(void);
 
 double a;
 double b;
@@ -19,12 +19,12 @@ int c;
 	do{
 	
 	
-	printf("select the option \n");
-	printf("1.find the rate\n");
+	printf("select \n");
+	printf("1.rate\n");
 	printf("2.Exit");
 	scanf("%d",&c);
 	switch(c){
-		case 1:userInput();break;
+		case 1:ui();break;
 		case 2:exit(0);
 	}
 	printf("\n\n");
@@ -32,34 +32,34 @@ int c;
 }while(c<3);
 }
 
-void userInput(){
+void ui(){
 	
-	
-	printf("enter the value of a");
+	//at=acess time et=execution time
+	printf("enter a");
 	scanf("%lf",&a);
-	printf("enter the value of b ");
+	printf("enter b ");
 	scanf("%lf",&b);
-	printf("enter memory access time");
+	printf("enter c");
 	scanf("%lf",&c);
-	printf("enter Percentage of time ");
-	scanf("%lf",d);
-	printf("enter effective time");
+	printf("enter time");
+	scanf("%lf",&d);
+	printf("enter e");
 	scanf("%lf",&e);
 
 	n = (a*1000000);
 	nm= (b*1000000);
 	f = (d/100);   
-	printf("page fault rate calculated for:\n");
-	printf("service page fault=%lf \n",n);
-	printf("service page fault modified page %lf \n",nm);
-	printf("memory access time %lf\n",c);
-	printf("effective access time %lf\n",e);
-        p=  page_fault_rate(n,nm,c,f,e);
-	printf("maximum acceptable page fault  = %.2e",p);
+	printf(" fault cal for:\n");
+	printf("spf=%lf \n",n);
+	printf("spf %lf \n",nm);
+	printf("mat %lf\n",c);
+	printf("eat %lf\n",e);
+    p=  pfr(n,nm,c,f,e);
+	printf("max apf  = %.2e",p);
 
 }
 
-double page_fault_rate(double a,double b,double mem,double t,double e){
+double pfr(double a,double b,double mem,double t,double e){
 double assume,serve;
 	double nu,de;
 	double pageFault;
